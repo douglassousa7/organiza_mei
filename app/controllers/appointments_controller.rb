909f837304cba38d68659a6d5deeec5c76737500
Douglas <douglas7.dsc@gmail.com>
2025-05-20 11:46:16 -0300

    Corrige ome do campo de agendamento de scheduled_at para scheduled_for e visualização de data agendada na listagem de agendamentos

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,51 @@
 class AppointmentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_appointment, only: %i[show edit update destroy]
+
+  def index
+    @appointments = current_user.appointments.includes(:client)
+  end
+
+  def show
+  end
+
+  def new
+    @appointment = current_user.appointments.build
+  end
+
+  def edit
+  end
+
+  def create
+    @appointment = current_user.appointments.build(appointment_params)
+
+    if @appointment.save
+      redirect_to @appointment, notice: "Agendamento criado com sucesso."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @appointment.update(appointment_params)
+      redirect_to @appointment, notice: "Agendamento atualizado com sucesso."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @appointment.destroy!
+    redirect_to appointments_url, notice: "Agendamento removido com sucesso."
+  end
+
+  private
+
+  def set_appointment
+    @appointment = current_user.appointments.find(params[:id])
+  end
+
+  def appointment_params
+    params.require(:appointment).permit(:title, :description, :scheduled_for, :client_id)
+  end
 end
